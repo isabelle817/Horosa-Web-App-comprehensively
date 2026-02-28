e86d073d7d713bb60673e8d8e8cd5fdfdbf70f26
HaLambur <190474635+HaLambur@users.noreply.github.com>
2026-02-27 21:26:29 -0800

    导出修复 + 三式合一结构 + 占星悬浮 + 本地服务常驻

--- Prepare_Runtime_Windows.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-setlocal EnableExtensions
-"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -File "%~dp0Prepare_Runtime_Windows.ps1"
-endlocal
